a82995583c713516ba982ef5ac5203a0ed8cb8ec
cat23123 <58714163+cat23123@users.noreply.github.com>
2023-09-16 22:46:20 -0400

    Creating post endpoint

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from db3 import add_event_to_table
 import cohere
 import datetime, re, json
 
@@ -13,9 +14,9 @@ keyWords = ["Start time:", "End time:"]
 #     End time: YYYY-MM-DD-HH-mm format"
 
 dateFormatStr = "Please schedule these events in an array. Each element should have the following JSON format: \
-    Title: text format\
-    Start time: YYYY-MM-DD HH:mm format \
-    End time: YYYY-MM-DD HH:mm format."
+    title: text format\
+    start: YYYY-MM-DD HH:mm format \
+    end: YYYY-MM-DD HH:mm format."
 
 now = datetime.datetime.now()
 nowStr = "The current date is " + now.strftime("%B %d, %Y.")
@@ -27,76 +28,63 @@ def generate_calendar_dates(prompt):
         prompt=prompt + nowStr + " " + dateFormatStr,
         max_tokens=2000
     )
-    events = parse_calendar_dates(response[0].text)
-    return generate_JSON(events)
+    return parse_calendar_dates(response[0].text)
 
 
 # parseCalendarDates parses the response from the model and generates dates
 def parse_calendar_dates(response):
     print(response)
     # remove all newlines
-    remove = re.sub('\n', '', response)
+    # remove = re.sub('\n', '', response)
 
     # remove leading and trailing text
-    leading_index = remove.index('Title:')
-    remove  = remove[leading_index:]
+    leading_index = response.index('[')
+    json_string  = response[leading_index:]
 
-    trailing_index = remove.rfind("End time: ") + 26
-    remove = remove[:trailing_index]
+    trailing_index = json_string.rfind("]") + 1
+    json_string = json_string[:trailing_index]
 
     # replace "Start time:" and "End time:"
-    remove = remove.replace("Start time:", "|")
-    remove = remove.replace("End time:", "|")
+    # remove = remove.replace("Start time:", "|")
+    # remove = remove.replace("End time:", "|")
 
     # split by title
-    events = remove.split("Title:")
+    # events = remove.split("Title:")
 
     # remove empty string
-    events = [x for x in events if x != '']
+    # events = [x for x in events if x != '']
 
     # split events by start time and end time
-    events = [event.split("|") for event in events]
+    # events = [event.split("|") for event in events]
 
     # strip leading and trailing whitespace
-    events = [s.strip() for event in events for s in event]
-    
-    return events
+    # events = [s.strip() for event in events for s in event]
 
+    print(json_string)
 
-def generate_JSON(events):
-    events_dict = []
-    for i in range(0, len(events), 3):
-        ds = {
-            "title": events[i],
-            "start": events[i+1],
-            "end": events[i+2],
-        }
-        events_dict.append(ds)
+    json_object = json.loads(json_string)
 
-    events_json = json.dumps(events_dict)
-    # print(events_json)
-    return events_json
+    for ob in json_object:
+        add_event_to_table(ob["title"], ob["start"], ob["end"])
 
+    return json_object
+  
 
-def filter_dates (s):
-    for w in keyWords:
-        if s.find(w):
-            return True
-    return False    
-
-@app.route('/new', methods=['POST', 'GET'])
-def flask_app():
+@app.route('/new', methods=['POST'])
+def endpoint_post():
     prompt = request.form.get('prompt')
     n_prompt = request.form.get('negative')
-
-    # print(prompt)
-    # print(n_prompt)
 
     res = generate_calendar_dates(prompt + " " + n_prompt)
 
     return res
 
+@app.route('/get', methods=['GET'])
+def endpoint_get():
+    res = generate_calendar_dates(prompt + " " + n_prompt)
+
+    return res
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
-
-# generate_calendar_dates("I like to game at night. I also have a work meeting at 5pm")
